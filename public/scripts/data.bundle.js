8698ffa8e2bda1ba845adcd14c15a2ac8d1fd14a
Geoffrey Neal <geoffrey_neal@hotmail.com>
2016-06-02 16:24:39 +0100

    commit changes before switching branches

--- public/scripts/data.bundle.js
+++ public/scripts/data.bundle.js
@@ -8,14 +8,15 @@ webpackJsonp([0],[
 
 	angular.module("weatherApp", []);
 
-	__webpack_require__(3);
 	__webpack_require__(4);
 	__webpack_require__(5);
+	__webpack_require__(6);
 
 /***/ },
 /* 1 */,
 /* 2 */,
-/* 3 */
+/* 3 */,
+/* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var angular = __webpack_require__(1);
@@ -50,7 +51,7 @@ webpackJsonp([0],[
 	});
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -67,7 +68,7 @@ webpackJsonp([0],[
 	});
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var angular = __webpack_require__(1);
