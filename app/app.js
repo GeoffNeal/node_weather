0fb391d9d4a13265a834459befe45b1149bbcfd5
Geoffrey Neal <geoffrey_neal@hotmail.com>
2016-06-02 16:21:02 +0100

    Added bootstrap, using bootstrap css CDN and adding angular-bootstrap-npm as a dependency.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var angular = require('angular');
+require( 'angular-bootstrap-npm' );
 
-angular.module("weatherApp", []);
+angular.module("weatherApp", ['ui.bootstrap']);
 
 require("./controllers/mainCtrl.js");
 require("./directives/graph.js");
